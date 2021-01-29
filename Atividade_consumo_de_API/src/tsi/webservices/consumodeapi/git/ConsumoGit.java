9048e3c6f752800e8868ae72b3025166f7a2536d
Edmilson <44274029+EdmilsonOSJr@users.noreply.github.com>
2021-01-29 17:51:27 -0300

    Update ConsumoGit.java

--- Atividade_consumo_de_API/src/tsi/webservices/consumodeapi/git/ConsumoGit.java
+++ Atividade_consumo_de_API/src/tsi/webservices/consumodeapi/git/ConsumoGit.java
@@ -7,6 +7,7 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -15,37 +16,52 @@ import com.google.gson.reflect.TypeToken;
 public class ConsumoGit {
 
 	public static void main(String[] args) {
-		
+		String usuario;
 		try {
 			
-			URL url = new URL("https://api.github.com/users/EdmilsonOSJr/followers");
-			URLConnection connection = url.openConnection();
-			BufferedReader entrada = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			Scanner input = new Scanner(System.in);			
 			
-			Gson gson = new Gson();
-			
-			
-			List<Map<String, String>> map = gson.fromJson(entrada, new TypeToken<List<Map<String, String>>>() {}.getType());
-			entrada.close();
-			
-			
-			System.out.println("Número de seguidores: "+map.size());
-			
-			for(Map<String, String> follower : map) {
+			while(true) {
 				
-				System.out.println("Nome do seguidor: "+follower.get("login"));
+				System.out.println("\nDigite o nome de um usuário do GitHub ou \"exit\" para sair:");
 				
-				url = new URL(" https://api.github.com/users/"+follower.get("login")+"/repos?type=owner\r\n");
-				connection = url.openConnection();
-				entrada = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+				usuario = input.nextLine();
 				
-				List<Map<Object, Object>> map2 = gson.fromJson(entrada, new TypeToken<List<Map<Object, Object>>>() {}.getType());
+				if(usuario.equalsIgnoreCase("exit")) {
+					input.close();
+					break;
+				}
+				
+				URL url = new URL("https://api.github.com/users/"+usuario+"/followers");
+				URLConnection connection = url.openConnection();
+				BufferedReader entrada = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+				
+				Gson gson = new Gson();
+				
+				
+				List<Map<String, String>> map = gson.fromJson(entrada, new TypeToken<List<Map<String, String>>>() {}.getType());
 				entrada.close();
 				
-				for(Map<Object, Object> repos : map2) {
-					System.out.println("\tRepositório: "+repos.get("name").toString());
+				
+				System.out.println("Número de seguidores: "+map.size());
+				
+				for(Map<String, String> follower : map) {
+					
+					System.out.println("Nome do seguidor: "+follower.get("login"));
+					
+					url = new URL(" https://api.github.com/users/"+follower.get("login")+"/repos?type=owner\r\n");
+					connection = url.openConnection();
+					entrada = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+					
+					List<Map<Object, Object>> map2 = gson.fromJson(entrada, new TypeToken<List<Map<Object, Object>>>() {}.getType());
+					entrada.close();
+					
+					for(Map<Object, Object> repos : map2) {
+						System.out.println("\tRepositório: "+repos.get("name").toString());
+					}
 				}
 			}
+			
 			//entrada.close();
 			
 		} catch (IOException e) {
